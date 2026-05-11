0b102a1fb44b40f28425c6a3dc4f24b9146a74a0
Lifa <holifatul.hsnh55@gmail.com>
2026-05-11 18:44:40 +0700

    update project

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,13 @@
-<?php
-
+use App\Http\Controllers\CartController;
+use App\Http\Controllers\CheckoutController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
 use App\Models\Product;
 
 /*
-|----------------------------------------
-| HOME ROUTE
-|----------------------------------------
+|-------------------------
+| HOME
+|-------------------------
 */
 Route::get('/', function () {
 
@@ -21,4 +21,21 @@ Route::get('/', function () {
 
     $products = Product::all();
     return view('dashboard', compact('products'));
+});
+
+
+/*
+|-------------------------
+| AUTH + CART (CUSTOMER)
+|-------------------------
+*/
+Route::middleware('auth')->group(function () {
+
+    Route::get('/cart', [CartController::class, 'index']);
+
+    // ✅ INI WAJIB ADA (FIX ERROR cart.add not defined)
+    Route::post('/cart/add/{id}', [CartController::class, 'addToCart'])
+        ->name('cart.add');
+
+    Route::post('/checkout', [CheckoutController::class, 'store']);
 });
